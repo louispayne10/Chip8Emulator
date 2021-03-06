3bef89cf066ffa42102ef6b82498eaf6749a36a0
Louis Payne <louispayne10@hotmail.co.uk>
2021-03-06 16:54:42 +0000

    Improved error handling in StaticStack.
    Previously we could not check if the stack was empty or full. Calling top() on an empty stack would have been UB. We check in top() if the stack is empty. Added member functions for empty and full.

--- src/StaticStack.h
+++ src/StaticStack.h
@@ -6,22 +6,33 @@
 class StaticStack {
 public:
     void push(uint16_t val) {
-        if (stack_ptr == 16)
-            throw std::runtime_error("stack has reached maximum size (15)");
+        if (full())
+            throw std::runtime_error("stack has reached maximum size");
 
         stack[stack_ptr] = val;
         stack_ptr++;
     }
 
     void pop() {
-        if (stack_ptr == 0)
+        if (empty())
             throw std::runtime_error("cannot pop from an empty stack");
 
         stack_ptr--;
     }
 
-    uint16_t top() const noexcept {
+    uint16_t top() const {
+        if (empty())
+            throw std::runtime_error("cannot get top from empty stack");
+
         return stack[stack_ptr - 1];
+    }
+
+    [[nodiscard]] bool empty() const noexcept {
+        return stack_ptr == 0;
+    }
+
+    [[nodiscard]] bool full() const noexcept {
+        return stack_ptr == stack.size();
     }
 
 private:
